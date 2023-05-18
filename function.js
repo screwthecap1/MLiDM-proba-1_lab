150b963056289a1fad4f69ce7849c2e41af8c53b
Klim Borodin <123664193+screwthecap1@users.noreply.github.com>
2023-05-18 23:40:18 +0400

    Update function.js

--- function.js
+++ function.js
@@ -1,113 +1,118 @@
-function validateArr(el) {
-    let result = false;
-    if ((el[0] % 2 == 0) && ( (el[1] >= 'A' && el[1] <= 'Z') || (el[1] >= 'a' && el[1] <= 'z') ) && (el[2] % 2 == 0) && (el[3] % 2 != 0)) {
-        result = true;
+//валидация элементов
+function validateElement(element) {
+  return (
+    ((element[0] >= "A" && element[0] <= "Z") ||
+      (element[0] >= "a" && element[0] <= "z")) &&
+    element[1] >= "0" &&
+    element[1] <= "9" &&
+    element[2] >= "0" &&
+    element[2] <= "9" &&
+    element[3] % 2 == 0
+  );
+}
+//объединение массивов
+function unionMas(mas1, mas2) {
+  let uMas = Array.from(new Set(mas1.concat(mas2)));
+  document.getElementById("union").innerHTML =
+    "Объединение множеств: " + "{" + uMas + "}";
+}
+//пересечение массивов
+function intersectionMas(mas1, mas2) {
+  let intMas = mas1.concat(mas2);
+  let intMas1 = Array.from(new Set(intMas));
+  let intMasR = new Array();
+  for (let i = 0; i < intMas1.length; i++) {
+    let q = 0;
+    for (let j = i; j < intMas.length; j++) {
+      if (intMas[j] == intMas1[i]) {
+        q++;
+      }
     }
-    return result;
+    if (q >= 2) {
+      intMasR.push(intMas1[i]);
+    }
+  }
+  document.getElementById("intersection").innerHTML =
+    "Пересечение множеств: " + "{" + intMasR + "}";
 }
 
-function unionArr(arr1, arr2) {
-    let uArr = Array.from(new Set(arr1.concat(arr2)));
-    document.getElementById("union").innerHTML = "Объединение множеств: " + "{" + uArr + "}";
+//дополнения множеств
+function additionMas(mas1, mas2) {
+  let addMas1 = Array.from(mas1);
+  let addMas2 = Array.from(mas2);
+
+  // a/b
+  for (let i = 0; i < mas2.length; i++) {
+    if (addMas1.indexOf(mas2[i], 0) != -1)
+      addMas1.splice(addMas1.indexOf(mas2[i], 0), 1);
+  }
+
+  // b/a
+  for (let i = 0; i < mas1.length; i++) {
+    if (addMas2.indexOf(mas1[i]) != -1)
+      addMas2.splice(addMas2.indexOf(mas1[i], 0), 1);
+  }
+
+  document.getElementById("addition").innerHTML =
+    "Дополнение множеств А/В: " +
+    "{" +
+    addMas1 +
+    "} " +
+    "<br>" +
+    "Дополнение множеств B/A: " +
+    "{" +
+    addMas2 +
+    "}";
 }
 
-function intersectionArr(arr1, arr2) {
-    let intArr = arr1.concat(arr2);
-    let intArr1 = Array.from(new Set(intArr));
-    let intArrR = new Array();
-    for (let i = 0; i < intArr1.length; i++)
-    {
-        let fl = 0;
-        for (let j = i; j < intArr.length; j++)
-        {
-            if (intArr[j] == intArr1[i])
-            {
-                fl++;
-            }
-        }
-        if (fl == 2)
-        {
-            intArrR.push(intArr1[i]);
-        }
-    }
-    document.getElementById("intersection").innerHTML = "Пересечение множеств: " + "{" + intArrR + "}";
+//симметрическая разность
+function symmetricAddMas(mas1, mas2) {
+  let addMas1 = Array.from(mas1);
+  let addMas2 = Array.from(mas2);
+
+  // a/b
+  for (let i = 0; i < mas2.length; i++) {
+    if (addMas1.indexOf(mas2[i], 0) != -1)
+      addMas1.splice(addMas1.indexOf(mas2[i], 0), 1);
+  }
+
+  // b/a
+  for (let i = 0; i < mas1.length; i++) {
+    if (addMas2.indexOf(mas1[i]) != -1)
+      addMas2.splice(addMas2.indexOf(mas1[i], 0), 1);
+  }
+
+  // a/b + b/a
+  let symAddMas = addMas1.concat(addMas2);
+  document.getElementById("symmetricAdd").innerHTML =
+    "Симметрическая разность множеств: " + "{" + symAddMas + "}";
 }
 
-function additionArr(arr1, arr2) {
-    let addArr1 = Array.from(arr1);
-    let addArr2 = Array.from(arr2);
+function Main() {
+  let mas1 = document.getElementById("id_mas1").value;
+  let mas2 = document.getElementById("id_mas2").value;
+  let strError = "";
+  mas1 = mas1.split(" ");
+  mas2 = mas2.split(" ");
+  console.log(mas1);
+  for (let i = 0; i < mas1.length; i++) {
+    if (!validateElement(mas1[i]))
+      strError += "Ошибка в " + (i + 1) + " элементе первого массива<br>";
+  }
 
-    // a/b
-    for (let i = 0; i < arr2.length; i++)
-    {
-        if (addArr1.indexOf(arr2[i], 0) != -1) addArr1.splice(addArr1.indexOf(arr2[i], 0), 1);
-    }
+  for (let i = 0; i < mas2.length; i++) {
+    if (!validateElement(mas2[i]))
+      strError += "Ошибка в " + (i + 1) + " элементе второго массива<br>";
+  }
 
-    // b/a
-    for (let i = 0; i < arr1.length; i++)
-    {
-        if (addArr2.indexOf(arr1[i]) != - 1) delete addArr2.splice(addArr2.indexOf(arr1[i], 0), 1);
-    }
-
-    document.getElementById("addition").innerHTML = "Дополнение множеств А&#47В: " + "{" + addArr1 + "} " + '<br>' + "Дополнение множеств B&#47A: " + "{" + addArr2 + "}";
+  if (strError == "") {
+    document.getElementById("result").innerHTML = "Результат расчета: <br>";
+    unionMas(mas1, mas2);
+    intersectionMas(mas1, mas2);
+    additionMas(mas1, mas2);
+    symmetricAddMas(mas1, mas2);
+  } else {
+    document.getElementById("result").innerHTML = strError;
+  }
 }
 
-function symmetricAddArr(arr1, arr2) {
-    let addArr1 = Array.from(arr1);
-    let addArr2 = Array.from(arr2);
-
-    // a/b
-    for (let i = 0; i < arr2.length; i++)
-    {
-        if (addArr1.indexOf(arr2[i], 0) != -1) addArr1.splice(addArr1.indexOf(arr2[i], 0), 1);
-    }
-
-    // b/a
-    for (let i = 0; i < arr1.length; i++)
-    {
-        if (addArr2.indexOf(arr1[i]) != - 1) delete addArr2.splice(addArr2.indexOf(arr1[i], 0), 1);
-    }
-
-    // a/b + b/a
-    let symAddArr = addArr1.concat(addArr2);
-    document.getElementById("symmetricAdd").innerHTML = "Симметрическая разность множеств: " + "{" + symAddArr + "}";
-}
-
-function Main ()
-{
-    // получение данных с формы
-    let mas1 = document.getElementById('mas1').value;
-    let mas2 = document.getElementById('mas2').value;
-
-    // создание массивов
-    arr1 = arr1.split(' ');
-    arr2 = arr2.split(' ');
-
-    // валидация элементов массивов
-    let mess1 = "";
-    for (let i = 0; i < arr1.length; i++)
-    {
-        if (!validateArr(arr1[i]))
-        {
-            mess1 += "Ошибка в "+ (i + 1) + " элементе ";
-        }
-    }
-    document.getElementById("val1").innerHTML = mess1;
-
-    let mess2 = "";
-    for (let i = 0; i < arr2.length; i++)
-    {
-        if (!validateArr(arr2[i]))
-        {
-            mess2 += "Ошибка в "+ (i + 1) + " элементе ";
-        }
-    }
-    document.getElementById("val2").innerHTML = mess2;
-
-    if (mess1 == "" && mess2 == "") {
-        unionArr(arr1, arr2);
-        intersectionArr(arr1, arr2);
-        additionArr(arr1, arr2);
-        symmetricAddArr(arr1, arr2);
-    }
-}
